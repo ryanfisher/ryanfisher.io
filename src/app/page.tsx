aa647c8f8b5b722e58d1c0de4f1f75de79e82d2d
Ryan Fisher <ryancfisher@gmail.com>
2025-11-12 22:15:51 -0800

    fix layout in mobile and other sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,38 +14,36 @@ const WorkItemBullet = ({company, position, dates, ...props}: WorkItemProps) => 
 )
 
 const WorkItemList = ({positions}: {positions: WorkItemProps[]}) => (
-  <ul role="list" className="divide-y divide-white/5">
+  <ul role="list" className="">
     {positions.map(position => <WorkItemBullet key={position.dates} {...position} />)}
   </ul>
 )
 
 export default function Home() {
   return (
-    <div className="font-sans justify-items-center p-8 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans p-8">
+      <main className="flex flex-col gap-[32px]">
         <h1 className={`text-4xl ${headlineBold.className}`}>Hi, I'm Ryan</h1>
-        <p className="w-3xl">
+        <p className="md:w-2xl lg:w-3xl">
           I'm a software engineer based in San Diego. I specialize in building web applications using
           Ruby on Rails on the backend and JavaScript, TypeScript, and React on the frontend. Over the
           past decade plus of writing software professionally, I've used a lot of different technologies,
           with many years of experience building applications with PostgreSQL, Sidekiq, Redis, etc.
         </p>
 
-        <p className="w-3xl">
+        <p className="md:w-2xl lg:w-3xl">
           When I'm not building software, I like to spend time outside, often still building. I've built my own
           office shed, a "treehouse" for my kids, a waterfall/pond (with more planned), and an ever-growing garden.
         </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="gap-4 items-center">
           <div>
-            <div className="px-4 sm:px-0">
-              <h2 className={`text-2xl mb-1 ${headlineBold.className}`}>Work</h2>
-            </div>
+            <h2 className={`text-2xl mb-1 ${headlineBold.className}`}>Work</h2>
             <WorkItemList positions={positions} />
           </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 gap-[24px] items-center justify-center">
       </footer>
     </div>
   );
